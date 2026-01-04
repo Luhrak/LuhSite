2b41482a927c7dc81812211e3c805e7dc3b399f5
Marcel Kähler <marcel.kaehler@stud.hs-flensburg.de>
2026-01-04 17:06:57 +0100

    Site 403 / 500, 500 try/catch in serverStatic

--- src/framework/middleware/serveStatic.js
+++ src/framework/middleware/serveStatic.js
@@ -2,6 +2,9 @@ import { serveDir } from "jsr:@std/http";
 import * as path from "jsr:@std/path";
 
 export const serveStatic = async (ctx) => {
+   try {
+  
+  
   if (ctx.status !== 404) {
     return ctx;
   } else if (path.extname(ctx.url.pathname)) {
@@ -19,4 +22,20 @@ export const serveStatic = async (ctx) => {
     }
   }
   return ctx;
+}
+ catch (err) {
+     try {
+ const html = await Deno.readTextFile("./src/templates/error500.html");
+
+      ctx.status = 500;
+      ctx.headers.set("Content-Type", "text/html; charset=utf-8");
+      ctx.body = html;
+    } catch {
+      ctx.status = 500;
+      ctx.body = "Internal Server Error";
+    }
+    return ctx;
+  }
+
+  
 };
